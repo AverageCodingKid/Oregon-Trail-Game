3be67e79671741f29b0186ce850402f827c9df83
shealewis123 <136538818+shealewis123@users.noreply.github.com>
2023-08-02 13:19:54 -0400

    changes

--- maingame.py
+++ maingame.py
@@ -5,7 +5,7 @@ chosen_intro = input("""\nOptions:
 2| Learn About the creator
 3| Source code
 4| Feedback
-                                Type here: """)
+                        Type here: """)
 
 if chosen_intro == "2":
     print("""Hello, I am Shea. 
@@ -16,11 +16,13 @@ YouTube: @-Meme-Master-
 GitHub: @AverageCodingKid
 """)
 elif chosen_intro == "1":
+    # Health
     player1_health = 100
     player2_health = 100
     player3_health = 100
     player4_health = 100    
-
+    # Food
+    food = 100
     total_health = player1_health + player2_health + player3_health + player4_health
 
     print("\nWelcome to the Oregon trail!", "\n\nMake sure to check out my YouTube channel @-Meme-Master-")
@@ -36,21 +38,24 @@ elif chosen_intro == "1":
     player3 = name3
     player4 = name4
 
-    store = input("""\n\n\nWhat would you like to purchase?      
+    trail_start_screen = input("""Select an option
+1| Begin journey
+2| Purchase items
+3| Quit game
+                        Type here: """)
+
+    if trail_start_screen == "2":
+        store = input("""\n\n\nWhat would you like to purchase?
+         
 1| Food
 2| Water
 3| Ox
 4| Clothes
 5| HP
 6| Wagon Wheels
-Type here: """)
+                        Type here: """)
 
-    # Health
-
-    # Total Health
-
-    # Food
-    food = 100
-
-else:
-    print("\nFeature in progress")
+elif chosen_intro == "3":
+    print("\nYou may find the source code here: https://raw.githubusercontent.com/AverageCodingKid/Oregon-Trail-Game/main/maingame.py\n")
+elif chosen_intro == "4":
+    print("\nYou may submit feedback here: https://docs.google.com/forms/d/1r7coffgHkjaSxmx5NR1jm5iR57ZGhlicuafjfvuwcx0")
