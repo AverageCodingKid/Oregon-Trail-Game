f41ae79edec76571895011ff6317f87fd9f86a4e
shealewis123 <136538818+shealewis123@users.noreply.github.com>
2023-08-02 08:26:14 -0400

    oregon trail game

--- maingame.py
+++ maingame.py
@@ -1,12 +1,11 @@
-# Intro options
 print("Oregon Trail Game")
-chosen_intro = input("""Options:
+chosen_intro = input("""\nOptions:
+
 1| Play the game
 2| Learn About the creator
 3| Source code
 4| Feedback
-
-Type here: """)
+                                Type here: """)
 
 if chosen_intro == "2":
     print("""Hello, I am Shea. 
@@ -16,51 +15,42 @@ Social:
 YouTube: @-Meme-Master-
 GitHub: @AverageCodingKid
 """)
-
-
-if chosen_intro == "1":
-
-    
-
-        
+elif chosen_intro == "1":
     player1_health = 100
     player2_health = 100
     player3_health = 100
     player4_health = 100    
 
-    total_health = player1_health + player2_health + player3_health + player4_health   
-        
+    total_health = player1_health + player2_health + player3_health + player4_health
 
-print("\nWelcome to the Oregon trail", "!", "\n\nMake sure to check out my YouTube channel @-Meme-Master-")
+    print("\nWelcome to the Oregon trail!", "\n\nMake sure to check out my YouTube channel @-Meme-Master-")
 
-# Players
-name1 = str(input("\nWhat would you like to name player 1 (You): "))
-name2 = str(input("What would you like to name player 2: "))
-name3 = str(input("What would you like to name player 3: "))
-name4 = str(input("What would you like to name player 4: "))        
+    # Players
+    name1 = input("\nWhat would you like to name player 1 (You): ")
+    name2 = input("What would you like to name player 2: ")
+    name3 = input("What would you like to name player 3: ")
+    name4 = input("What would you like to name player 4: ")        
 
-player1 = name1    
-player2 = name2
-player3 = name3
-player4 = name4
+    player1 = name1    
+    player2 = name2
+    player3 = name3
+    player4 = name4
 
-store = input("""\n\n\nWhat would you like to purchase?
-              
-1| Food.
+    store = input("""\n\n\nWhat would you like to purchase?      
+1| Food
 2| Water
 3| Ox
 4| Clothes
 5| HP
 6| Wagon Wheels
-              Type here: """)
-    
+Type here: """)
+
     # Health
 
-    
     # Total Health
 
-    
     # Food
-food = 100
+    food = 100
 
-
+else:
+    print("\nFeature in progress")
