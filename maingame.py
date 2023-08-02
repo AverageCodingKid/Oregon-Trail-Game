063e9d44adb47ae9c0e2e1c1f40c563ba1869fb9
shealewis123 <136538818+shealewis123@users.noreply.github.com>
2023-08-02 08:18:50 -0400

    oregon trail

--- maingame.py
+++ maingame.py
@@ -9,7 +9,7 @@ chosen_intro = input("""Options:
 Type here: """)
 
 if chosen_intro == "2":
-    print(""" Hello, I am Shea. 
+    print("""Hello, I am Shea. 
 I started computer programming at the age of 10. More coming soon...
 --------------------------------------------------------------------
 Social:
@@ -18,36 +18,49 @@ GitHub: @AverageCodingKid
 """)
 
 
+if chosen_intro == "1":
 
-    # Introduction
-    player1 = str(input("What would you like to name player 1 (You): "))
-    name2 = str(input("What would you like to name player 2: "))
-    name3 = str(input("What would you like to name player 3: "))
-    name4 = str(input("What would you like to name player 4: "))
     
-    # Players
-    player2 = name2
-    player3 = name3
-    player4 = name4
-    
-    # Health
+
+        
     player1_health = 100
     player2_health = 100
     player3_health = 100
-    player4_health = 100
+    player4_health = 100    
+
+    total_health = player1_health + player2_health + player3_health + player4_health   
+        
+
+print("\nWelcome to the Oregon trail", "!", "\n\nMake sure to check out my YouTube channel @-Meme-Master-")
+
+# Players
+name1 = str(input("\nWhat would you like to name player 1 (You): "))
+name2 = str(input("What would you like to name player 2: "))
+name3 = str(input("What would you like to name player 3: "))
+name4 = str(input("What would you like to name player 4: "))        
+
+player1 = name1    
+player2 = name2
+player3 = name3
+player4 = name4
+
+store = input("""\n\n\nWhat would you like to purchase?
+              
+1| Food.
+2| Water
+3| Ox
+4| Clothes
+5| HP
+6| Wagon Wheels
+              Type here: """)
+    
+    # Health
+
     
     # Total Health
-    total_health = player1_health + player2_health + player3_health + player4_health
+
     
     # Food
-    food = 100
-    
-    # Play game
-    if chosen_intro == "1":
-        print("Oregon Trail")
-        print("Welcome to the Oregon trail", player1, "!", "\nMake sure to check out my YouTube channel @-Meme-Master-")
-        
-        # Add condition here to break the loop
-        
-        # Test successful if this point is reached
-        print("\nTest Complete...")
+food = 100
+
+
